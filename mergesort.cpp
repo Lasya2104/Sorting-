cb3863b71ae5c18d055b79dff8ade47b901d7a73
Lasya2104 <24B11DS217@adityauniversity.in>
2025-02-15 11:20:23 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void merge(int a[],int n,int start,int mid,int end)
 {
-	int i=start;
+	int i=start; 
 	int j=mid+1;
-	int b[end-start+1];
+	int b[end-start+1]; // this stores the sorted values
 	int k=0;
 	while(i<=mid && j<=end)
 	{
@@ -16,10 +16,10 @@ void merge(int a[],int n,int start,int mid,int end)
 			b[k++]=a[j++];
 		}
 	}
-	while(i<=mid)b[k++]=a[i++];
+	while(i<=mid)b[k++]=a[i++]; // if single element remains it automatically stores
 	while(j<=end)b[k++]=a[j++];
 	k=0;
-	for(int i=start;i<=end;i++)
+	for(int i=start;i<=end;i++) // storing the sorted values to the original array
 	{
 		a[i]=b[k++];
 	}
@@ -28,9 +28,9 @@ void mergesort(int a[],int n,int start,int end)
 {
 	if(start>=end)return;
 	int mid=(start+end)/2;
-	mergesort(a,n,start,mid);
-	mergesort(a,n,mid+1,end);
-	merge(a,n,start,mid,end);
+	mergesort(a,n,start,mid); // it sorts the first half of the array
+	mergesort(a,n,mid+1,end); // it sorts the second half of the array
+	merge(a,n,start,mid,end); // it merges the two sorted arrays
 }
 int main()
 {
